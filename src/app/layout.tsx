e51a89074ceb65e1d21863438965c2f64b8576af
Akanksha Pilli <97347586+Akankshapilli@users.noreply.github.com>
2025-10-20 17:38:14 +0530

    Remove head section from layout component
    Removed head section with preconnect link for fonts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,13 +105,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-      </head>
       <body
         className={`${libreBaskerville.className} ${poppins.variable} ${inter.variable} ${playfairDisplay.variable} ${lato.variable} ${lora.variable} ${josefinslab.variable} ${hind.variable} bg-white text-black dark:bg-black dark:text-white transition-colors duration-300`}
       >
